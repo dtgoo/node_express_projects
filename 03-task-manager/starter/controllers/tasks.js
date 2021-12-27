0238ddced56b66346cb337808f24daefa8cdce91
Jesse Perez <jessep1130@gmail.com>
2021-12-27 16:37:12 -0600

    created other tasks routes - moving on to database

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -9,7 +9,22 @@ const getAllTasks = (req,res) => {
 const createTask = (req,res) => {
     res.send('Task has been created');
 }
+
+const getTask = (req,res) => {
+    res.send('get single task');
+}
+
+const updateTask = (req,res) => {
+    res.send('update tasks');
+}
+
+const deleteTask = (req,res) => {
+    res.send('delete task');
+}
 module.exports = {
     getAllTasks,
     createTask,
+    getTask,
+    updateTask,
+    deleteTask,
 }
