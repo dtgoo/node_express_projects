d1f54ab79441102cae8c3f78ae1abbcd8dcce08e
Jesse Perez <jessep1130@gmail.com>
2021-12-27 16:34:40 -0600

    creating more functions for api routes

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -3,9 +3,13 @@
 
 
 const getAllTasks = (req,res) => {
-    res.send('All items')
+    res.send('All items from the file')
 }
 
+const createTask = (req,res) => {
+    res.send('Task has been created');
+}
 module.exports = {
     getAllTasks,
+    createTask,
 }
