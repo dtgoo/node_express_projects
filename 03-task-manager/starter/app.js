0a5981b5aeeec7e7da0e7c22e9de721a04dde024
Jesse Perez <jessep1130@gmail.com>
2021-12-28 17:22:29 -0600

    successful database connection

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,7 +1,8 @@
-require('./db/connect')
+
 const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks')
+const connectDB = require('./db/connect')
 
 // middleware
 app.use(express.json());
@@ -15,15 +16,16 @@ app.get('/hello', (req, res) => {
 app.use('/api/v1/tasks', tasks)
 
 
-
-//app.get('/api/v1/tasks') - get all tasks
-//app.post('/api/v1/tasks') - post a new task
-//app.get('/api/v1/tasks/:id') - get a single task by id 
-//app.path('/api/v1/tasks/:id') - update task
-//app.delete('/api/v1/tasks/:id') - delete a task by ID
-
-
 //creating a port number and telling the browser to listen on that port number
 const port = 3000; 
-app.listen(port, console.log(`SERVER IS LISTENING ON PORT ${port}...`));
+const start = async () => {
+ try {
+    await connectDB()
+    app.listen(port, console.log(`SERVER IS LISTENING ON PORT ${port}...`));
 
+ } catch (err) {
+    console.log(err);
+ }
+}
+
+start()
--- 03-task-manager/starter/db/connect.js
+++ 03-task-manager/starter/db/connect.js
@@ -3,14 +3,15 @@ const mongoose = require('mongoose');
 const connectionString = "mongodb+srv://mongoDBuser:Casio414@nodeexpressprojects.jo4fb.mongodb.net/03-TASK-MANAGER?retryWrites=true&w=majority"
 
 
-mongoose
-    .connect(connectionString,{
+
+const connectDB = (url) => {
+   return mongoose.connect(connectionString, {
         useNewUrlParser: true,
         useCreateIndex: true,
         useFindAndModify: false,
         useUnifiedTopology: true,
     })
-    .then(() => console.log('CONNECTED TO DB...'))
-    .catch((err) => console.log(err));
+}
 
 
+module.exports = connectDB
