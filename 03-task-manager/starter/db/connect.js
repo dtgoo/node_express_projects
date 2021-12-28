f1da8ff70dbb907e740b5b958995b07f9c3d6061
Jesse Perez <jessep1130@gmail.com>
2021-12-28 17:09:07 -0600

    database connection setup

--- 03-task-manager/starter/db/connect.js
+++ 03-task-manager/starter/db/connect.js
@@ -1,2 +1,16 @@
-const connectionString = "mongodb+srv://mongoDBuser:<password>@nodeexpressprojects.jo4fb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const mongoose = require('mongoose');
+
+const connectionString = "mongodb+srv://mongoDBuser:Casio414@nodeexpressprojects.jo4fb.mongodb.net/03-TASK-MANAGER?retryWrites=true&w=majority"
+
+
+mongoose
+    .connect(connectionString,{
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log('CONNECTED TO DB...'))
+    .catch((err) => console.log(err));
+
 
