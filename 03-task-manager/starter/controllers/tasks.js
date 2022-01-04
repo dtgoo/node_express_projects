e882d6fe3d1c6ac1f69b3abe6900ad91f253b7a0
Jesse Perez <jessep1130@gmail.com>
2022-01-03 21:52:39 -0600

    added error handling to controllers

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -5,8 +5,13 @@ const getAllTasks = (req,res) => {
 }
 
 const createTask = async (req,res) => {
-    const task = await Task.create(req.body)
-    res.status(201).json({ task });
+    try {
+        const task = await Task.create(req.body)
+        res.status(201).json({ task });
+    } catch (error) {
+        res.status(500).json({msg: error})
+    }
+
 }
 
 const getTask = (req,res) => {
@@ -17,7 +22,7 @@ const updateTask = (req,res) => {
     res.send('update tasks');
 }
 
-const deleteTask = (req,res) => {
+const deleteTask = async (req,res) => {
     res.send('delete task');
 }
 module.exports = {
--- 03-task-manager/starter/models/Task.js
+++ 03-task-manager/starter/models/Task.js
@@ -3,14 +3,14 @@ const mongoose = require('mongoose');
 const TaskSchema = new mongoose.Schema({
     name: {
         type: String,
-        require: [true, 'must provide a name'],
+        required: [true, 'must provide a name'],
         trim: true,
         maxlength: [20, 'name cannot be more than 20 characters']
     },
     completed: {
         type: Boolean,
         default: false,
-    }        
+    },        
 })
 
 module.exports = mongoose.model('Task', TaskSchema)
