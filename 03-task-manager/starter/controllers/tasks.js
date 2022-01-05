7b3ddd8a697415365b11406a6e99ff21075faa7f
Jesse Perez <jessep1130@gmail.com>
2022-01-04 23:01:33 -0600

    troubleshooting bugs

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -11,13 +11,13 @@ const getAllTasks = async (req,res) => {
         const tasks = await Task.find({})
         res.status(200).json({tasks});
     } catch (error) {
-        res.status(500).json({msg: error})
+        res.status(500).send({msg: error})
     }
 }
 const createTask = async (req,res) => {
     try {
         const task = await Task.create(req.body)
-        res.status(201).json(task);
+        res.status(201).json({task});
     } catch (error) {
         res.status(500).send({msg: error})
     }
@@ -39,5 +39,5 @@ module.exports = {
     createTask,
     getTask,
     updateTask,
-    deleteTask
+    deleteTask,
 }
