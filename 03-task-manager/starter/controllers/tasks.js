29304ce5eea535804ce29bbbccfb823327d6a4c7
Jesse Perez <jessep1130@gmail.com>
2022-01-10 21:34:08 -0600

    working on controllers

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -25,10 +25,15 @@ const createTask = async (req,res) => {
 
 const getTask = async (req,res) => {
     try {
-        const task = await Task.findOne({id: req.params.id});
-        res.send({id: req.params.id});
+        const { id:taskID } = req.params
+        const task = await Task.findOne({_id:taskID})
+        if(!task) {
+            return res.status(404).json({ msg:`no task with id: ${taskID}`})
+        }
+
+        res.status(200).json({task});
     } catch (error) {
-        res.status(500).send(error)
+        res.status(500).json({msg: error})
     }
 }
 
