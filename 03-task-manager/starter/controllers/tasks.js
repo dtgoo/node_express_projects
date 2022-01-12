80a15f6104ff463a81dbe96f41fb1fd06ff9ad87
Jesse Perez <jessep1130@gmail.com>
2022-01-11 21:39:08 -0600

    edited delete controller

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -44,11 +44,11 @@ const updateTask = (req,res) => {
 const deleteTask = async (req,res) => {
     try {
         const { id: taskID} = req.params
-        const task = await Task.deleteOne({_id:taskID})
+        const task = await Task.findOneAndDelete({_id:taskID})
         if(!task) {
             return res.status(404).json({msg: `no task with id: ${taskID}`})
         }
-        res.status(200).json({msg: 'Task has been deleted!'});
+        res.status(200).json({task});
     }catch(error) {
         res.status(500).json({msg: error})
     }
