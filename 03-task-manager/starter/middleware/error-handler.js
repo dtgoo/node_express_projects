7d3b60a79c29937bb8fca4b387ea88ab049a484b
Jesse Perez <jessep1130@gmail.com>
2022-01-19 22:42:46 -0600

    test commit

--- 03-task-manager/starter/middleware/error-handler.js
+++ 03-task-manager/starter/middleware/error-handler.js
@@ -7,4 +7,4 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     return res.status(err.status).json({ msg:err.message})
 }
 
-module.exports = errorHandlerMiddleware
+module.exports = errorHandlerMiddleware;
